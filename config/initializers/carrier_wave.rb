3e88d328107e6dc9db74081278a134e75067cad1
dmitrbayan <dmitrybayan@yandex.ru>
2020-07-31 15:43:32 +0300

    some magic with active_storage and carrier_wave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-# if Rails.env.production?
-#  CarrierWave.configure do |config|
-#    config.fog_credentials = {
-#      provider: 'AWS',
-#      aws_access_key_id: ENV['S3_ACCESS_KEY'],
-#      aws_secret_access_key: ENV['S3_SECRET_KEY'],
-#      aws_bucket: ENV['S3_BUCKET']
-#    }
-#    config.fog_directory = ENV['S3_BUCKET']
-#  end
-# end
+if Rails.env.production?
+ CarrierWave.configure do |config|
+   config.fog_credentials = {
+     provider: 'AWS',
+     aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+     aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+     aws_bucket: Rails.application.credentials.dig(:aws, :bucket)
+   }
+   config.fog_directory = ENV['S3_BUCKET']
+ end
+end
