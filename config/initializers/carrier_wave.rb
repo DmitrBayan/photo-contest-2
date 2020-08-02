825a3eb549351a8e454e585e029b3614260621c3
dmitrbayan <dmitrybayan@yandex.ru>
2020-08-02 17:58:23 +0300

    carrier wave configure

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,12 +3,13 @@
 # if Rails.env.production?
  CarrierWave.configure do |config|
    config.fog_credentials = {
-     provider: 'AWS',
-     aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+     provider:              'AWS',
+     aws_access_key_id:     Rails.application.credentials.dig(:aws, :access_key_id),
      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-     aws_bucket: Rails.application.credentials.dig(:aws, :bucket),
-     aws_region: Rails.application.credentials.dig(:aws, :region)
+     use_iam_profile:       false,
+     region:                Rails.application.credentials.dig(:aws, :region)
    }
    config.fog_directory = Rails.application.credentials.dig(:aws, :bucket)
+   config.fog_public    = false
  end
 # end
