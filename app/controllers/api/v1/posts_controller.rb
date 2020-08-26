28d9b0a6609bf47a1f3f8fd4db0fece847d02cdc
Dmitry <dmitrybayan@ya.ru>
2020-08-27 00:16:36 +0300

    create post api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,20 +1,21 @@
 module Api
   module V1
-    class PostsController < ::ApiController
+    class PostsController < ::Api::ApiController
       layout false
       before_action :verify_authenticity_token
 
       def index
-        @posts = Post.by_title_or_description(params[:search])
+        posts = Post.by_title_or_description(params[:search])
                      .or(Post.by_user_full_name(params[:search]))
                      .reorder(params[:sorting])
         render json: posts, status: :ok
       end
 
       def show
-        @post = Post.find(params[:id])
-        @comments = @post.comments
-        render json: post, status: :ok
+        post = Post.find(params[:id])
+        comments = post.comments
+        user = User.find(post.user_id)
+        render json: [user, post, comments], status: :ok
       end
 
       def create
@@ -23,11 +24,22 @@ module Api
       end
 
       def destroy
-        @post = current_user.posts.find(params[:id])
-        raise ::Errors::Unright unless @api_user.id == @post.user_id
+        post = Post.find(params[:id])
+        raise ::Errors::Unright unless @api_user.id == post.user_id
 
-        @post.destroy
+        post.destroy
         render json: { message: 'destroyed' }, status: :ok
+      end
+
+      def update
+        post = Post.find(params[:id])
+        raise ::Errors::Unright unless @api_user.id == post.user_id
+
+        if post.update(post_params)
+          render json: post, status: :ok
+        else
+          render json: {errors: post.errors}, status: :unprocessable_entity
+        end
       end
 
       private
