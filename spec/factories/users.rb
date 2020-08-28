7f66632d906965b6f380a6dd752124c198917596
Dmitry <dmitrybayan@ya.ru>
2020-08-28 18:34:33 +0300

    finish factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   factory :user do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-    image_url { Dir[Rails.root.join("spec/support/users/user_default.png")].to_s }
+    image_url { File.open("spec/support/users/user_default.png") }
     provider { 'vkontakte' }
     admin { Faker::Boolean.boolean(true_ratio: 0.1) }
     uid { Faker::Number.number(digits: 10) }
