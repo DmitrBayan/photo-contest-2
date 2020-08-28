817de7f0a0fac91c932b34c115ce03f49d5a3e35
Dmitry <dmitrybayan@ya.ru>
2020-08-28 15:52:53 +0300

    edit routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
-      resources :posts
-      resources :comments
-      resources :likes
+      resources :posts do
+        resources :comments
+        resources :likes
+      end
     end
   end
   get 'errors/error_404'
