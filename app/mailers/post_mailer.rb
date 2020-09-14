a9ebb37cb30ea43614e8db5ae6e2a582eb062052
Dmitry <dmitrybayan@ya.ru>
2020-09-15 00:41:02 +0300

    mailer fix

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -4,7 +4,7 @@ class PostMailer < ApplicationMailer
   default from: 'noreply@bayan-photo-contest.com'
 
   def state_change_email(post)
-    @user = User.find_by(id: post.user_id)
+    @user = post.user
     return if @user.email.blank?
 
     @post = post
