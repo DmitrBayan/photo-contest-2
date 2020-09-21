76ba20ac077c41ed0d80897d0a614ec3f6c53524
Dmitry <dmitrybayan@ya.ru>
2020-09-21 23:49:51 +0300

    fix errors

--- app/interactions/users/auth.rb
+++ app/interactions/users/auth.rb
@@ -26,7 +26,7 @@ module Users
     def from_vk
       user.first_name = auth_hash['info']['first_name'] if user.first_name.blank?
       user.last_name = auth_hash['info']['last_name'] if user.last_name.blank?
-      user.remote_image_url_url = auth_hash['info']['image'] if user.image_url.blank?
+      user.remote_image_url_url = auth_hash['extra']['raw_info']['photo_400_orig'] if user.image_url.blank?
       user.url = auth_hash['info']['urls']['Vkontakte']
       user.access_token = auth_hash['credentials']['token']
     end
