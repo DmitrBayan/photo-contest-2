6d6c15c52d562ba67d078896c25b2457a427511f
Dmitry <dmitrybayan@ya.ru>
2020-09-26 23:31:25 +0300

    show attached image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,12 @@
 
 //= require bootstrap
 //= require_tree.
+
+var loadFile = function (event) {
+    var output = document.getElementById('output');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function () {
+        URL.revokeObjectURL(output.src) // free memory
+    }
+    output.style['display'] = 'block';
+};
