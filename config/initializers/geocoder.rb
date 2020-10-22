c8de63811566ebac2d8bf1533d41208e17eabc57
Dmitry <dmitrybayan@ya.ru>
2020-10-22 15:00:28 +0300

    ipinfo cred

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -5,13 +5,13 @@ Geocoder.configure(
   # timeout: 3,                 # geocoding service timeout (secs)
   lookup: :google, # name of geocoding service (symbol)
   ip_lookup: :ipinfo_io, # name of IP address geocoding service (symbol)
-  language: :en, # ISO-639 language code
+  # language: :en, # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: 'AIzaSyAgD5jrmV7TZa1AWIKaRTzrjluLSRVph5E', # API key for geocoding service
+  api_key: Rails.application.credentials[:ipinfo_io], # API key for geocoding service
   cache: Redis.new, # cache object (must respond to #[], #[]=, and #del)
-  cache_prefix: 'geocoder:', # prefix (string) to use for all cache keys
+  # cache_prefix: 'geocoder:', # prefix (string) to use for all cache keys
 
   # Exceptions that should not be rescued by default
   # (if you want to implement custom error handling);
